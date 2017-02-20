915b6f7f4bb00b8c06965353a129ce088f246fae
虞冠群 <ygqdev0126@gmail.com>
2017-02-21 01:30:34 +0800

    framework

---
+++ Example/ContextMenu_iOS/ContextMenu_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ContextMenu_iOS.h
+//  ContextMenu_iOS
+//
+//  Created by 虞冠群 on 2017/2/21.
+//  Copyright © 2017年 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ContextMenu_iOS.
+FOUNDATION_EXPORT double ContextMenu_iOSVersionNumber;
+
+//! Project version string for ContextMenu_iOS.
+FOUNDATION_EXPORT const unsigned char ContextMenu_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContextMenu_iOS/PublicHeader.h>
+
+
